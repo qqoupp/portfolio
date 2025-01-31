700a705a5a4649d8840c58e3bf426e35b0ba1543
qqoupp <74557790+qqoupp@users.noreply.github.com>
2025-01-31 02:17:43 +0000

    added the background

--- app-portfolio/src/App.tsx
+++ app-portfolio/src/App.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import backgroundImage from './components/images/background/blurry-gradient-haikei.svg';
 import { ThemeProvider, CssBaseline} from "@mui/material";
 import theme from "./Theme/index";
 import MainRouter from "./components/routers/MainRouter";
 import Container from "./components/Container";
+
 function App() {
+  useEffect(() => {
+    document.body.style.backgroundImage = `url(${backgroundImage})`;
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundAttachment = 'fixed';
+
+    return () => {
+      document.body.style.backgroundImage = '';
+      document.body.style.backgroundRepeat = '';
+      document.body.style.backgroundSize = '';
+      document.body.style.backgroundAttachment = '';
+    };
+  }, [])
   return (
   <ThemeProvider theme={theme}>
     <CssBaseline  />
@@ -15,3 +30,4 @@ function App() {
 );
 }
 export default App;
+
